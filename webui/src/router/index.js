11a7b40decba6753364d98bbea5d9cacc63f3976
MarcoLinardi <marco.linardi02@gmail.com>
2025-05-20 17:16:30 +0200

    Index aggiornato

--- webui/src/router/index.js
+++ webui/src/router/index.js
@@ -1,13 +1,19 @@
 import {createRouter, createWebHashHistory} from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue'
+import ProfileView from '../views/ProfileView.vue'
+import NewGroupView from '../views/NewGroupView.vue'
+import UsersView from '../views/UsersView.vue'
 
 const router = createRouter({
 	history: createWebHashHistory(import.meta.env.BASE_URL),
 	routes: [
-		{ path: '/', redirect: '/login' },
-		{path: '/home', component: HomeView},
+		{path: '/', redirect: '/login' },
 		{path: '/login', component: LoginView},
+		{path: '/home', component: HomeView},
+		{path: '/profile', component: ProfileView},
+		{path: '/newGroup', component: NewGroupView},
+		{path: '/usersList', component: UsersView},
 		{path: '/pathmatch(.*)*', redirect: '/login'},
 	]
 })
