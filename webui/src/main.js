70f2d2f871d05062ddbbc49b1614469ddea7f48f
MarcoLinardi <marco.linardi02@gmail.com>
2025-06-03 20:27:37 +0200

    registrazione nuovi component

--- webui/src/main.js
+++ webui/src/main.js
@@ -5,15 +5,15 @@ import axios from './services/axios.js';
 import ErrorMsg from './components/ErrorMsg.vue'
 import LoadingSpinner from './components/LoadingSpinner.vue'
 import UserItem from './components/UserItem.vue';
-import UserProfile from './components/UserProfile.vue';
 import ChatItem from './components/ChatItem.vue';
 import ChatArea from './components/ChatArea.vue';
 import NewChat from './components/NewChat.vue';
+import Message from './components/Message.vue';
+import ChatInfo from './components/ChatInfo.vue';
+import ManageMember from './components/ManageMember.vue';
 
 import './assets/dashboard.css'
 import './assets/main.css'
-
-
 
 
 const app = createApp(App)
@@ -21,9 +21,11 @@ app.config.globalProperties.$axios = axios;
 app.component("ErrorMsg", ErrorMsg);
 app.component("LoadingSpinner", LoadingSpinner);
 app.component("UserItem", UserItem)
-app.component("UserProfile", UserProfile)
 app.component("ChatItem", ChatItem)
 app.component("ChatArea", ChatArea)
+app.component("ChatInfo", ChatInfo)
+app.component("ManageMember", ManageMember)
 app.component("NewChat", NewChat)
+app.component("Message", Message)
 app.use(router)
 app.mount('#app')
